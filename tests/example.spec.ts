128db29373504a002611349bbceb0e051795e938
Artem Reva <artemreva79@gmail.com>
2026-04-30 21:10:17 +0300

    Fix playwright test, regarding webserver

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("login", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+  await page.goto("/");
 
   // Click the get started link.
   await page.getByRole("button", { name: "Login" }).click();
